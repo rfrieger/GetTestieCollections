fcbd00eb5e9e9430dff5e2ef8710ff53f2add2a8
Robert Rieger <robertrieger@zipcoders-MacBook-Pro-96.local>
2019-06-09 08:32:41 -0400

    Iterator worked on

--- gettestie/src/main/java/rocks/zipcode/Person.java
+++ gettestie/src/main/java/rocks/zipcode/Person.java
@@ -1,10 +1,23 @@
 package rocks.zipcode;
 
 
-public class Person implements Comparable<Person> {
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+class Person implements Comparable<Person>{//, Iterable<Person> {
     private String name;
     private int yearOfBirth;
     private Address address;
+
+//    @Override
+//    public Iterator<Person> iterator() {
+//        return null;
+//    }
+//
+//    @Override
+//    public void forEach(Consumer<? super Person> action) {
+//        return;
+//    }
 
     /**
      * Create a person with given name and age.
--- gettestie/src/test/java/rocks/zipcode/IteratorTest.java
+++ gettestie/src/test/java/rocks/zipcode/IteratorTest.java
@@ -2,6 +2,7 @@ package rocks.zipcode;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.PriorityQueue;
 
@@ -10,16 +11,25 @@ import static org.junit.Assert.assertEquals;
 public class IteratorTest {
 
 
-//    @Test
-//    public void IteratorTest () {
-//
-//
-//        Person a = new Person("", 99);
-//        Person b = new Person("", 67);
-//        Person c = new Person("", 60);
-//
-//
-//
-//        assertEquals(true, pq.contains(c));
-//    }
+    @Test
+    public void IteratorTest  () {
+
+        ArrayList<Object> arr = new ArrayList();
+        Object a = new Person("", 99);
+        Object b = new Person("", 67);
+        Object c = new Person("", 60);
+
+        arr.add(a);
+        arr.add(b);
+        arr.add(c);
+
+
+//        for (Person p : arr) {
+//            p.getName() = "bob";
+//        }
+
+
+
+        assertEquals("bob", ((Person) c).getName());
+    }
 }
